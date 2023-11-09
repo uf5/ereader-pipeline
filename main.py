2d7d5e2d7d1568e6e5d8add850b4f20a31bcea68
uf5 <notbailey13@gmail.com>
2023-11-09 12:28:31 +0300

    Add argparse, use temporary directory

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 from PIL import Image
 from pathlib import Path
-from sys import argv
 import img2pdf
-
-
-def downscale(input_dir, output_dir, factor):
-    for f in (x for x in input_dir.iterdir() if x.is_file()):
-        new_file = output_dir / f.name
-        print(f"Processing: {f.name}")
-        img = Image.open(f)
-        img.thumbnail((int(img.width * factor), int(img.height * factor)), Image.Resampling.LANCZOS)
-        img.save(new_file, img.format)
-
-
-def generate_pdf(input_dir, output_file):
-    with open(output_file, "wb") as f:
-        f.write(img2pdf.convert(sorted([str(x) for x in input_dir.iterdir() if x.is_file()])))
+import argparse
+import zipfile as zf
+import tempfile as tf
 
 
 if __name__ == "__main__":
-    input_dir = Path(argv[1])
+    parser = argparse.ArgumentParser(
+        prog="ereader-pipeline", description="downscale and convert .cbz to .pdf"
+    )
+    parser.add_argument("input")
+    parser.add_argument("output")
+    parser.add_argument("-f", "--factor", type=float, default=0.7, metavar="f")
 
-    downscale_dir = input_dir / "downscaled"
-    downscale_dir.mkdir(parents=True, exist_ok=True)
+    args = parser.parse_args()
 
-    factor = float(argv[2])
+    input_file = Path(args.input)
+    output_file = Path(args.output)
+    factor = args.factor
 
-    pdf_file = Path(argv[3])
-
-    downscale(input_dir, downscale_dir, factor)
-    print("Downscaling complete.")
-    generate_pdf(downscale_dir, pdf_file)
-    print(f"Output pdf file: {pdf_file}")
-
+    with tf.TemporaryDirectory() as dir_scans:
+        # extract
+        with zf.ZipFile(input_file) as zfile:
+            zfile.extractall(dir_scans)
+        # downscale
+        with tf.TemporaryDirectory() as dir_downscaled:
+            dir_downscaled_p = Path(dir_downscaled)
+            for page in filter(Path.is_file, Path(dir_scans).iterdir()):
+                downscaled_path = dir_downscaled_p / page.name
+                print(f"processing: {page.name}")
+                img = Image.open(page)
+                img.thumbnail(
+                    (int(img.width * factor), int(img.height * factor)),
+                    Image.Resampling.LANCZOS,
+                )
+                img.save(downscaled_path, img.format)
+            print("downscaling complete")
+            # combine images into pdf
+            with open(output_file, "wb") as f:
+                f.write(img2pdf.convert(sorted([str(x) for x in dir_downscaled_p.iterdir() if x.is_file()])))  # type: ignore
+            print(f"output pdf file: {output_file}")
